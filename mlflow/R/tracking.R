a26eb23f7d09337fa86a6d43b5c20a0530726d4d
Javier Luraschi <javierluraschi@hotmail.com>
2018-09-19 16:06:27 -0700

    add mlflow talk

---
+++ mlflow/R/tracking.R
@@ -0,0 +1,15 @@
+# !source mlflow::mlflow_run(entry_point = .file)
+
+library(mlflow)
+
+# Log a parameter (key-value pair)
+mlflow_log_param("param1", 5)
+
+# Log a metric; metrics can be updated throughout the run
+mlflow_log_metric("foo", 1)
+mlflow_log_metric("foo", 2)
+mlflow_log_metric("foo", 3)
+
+# Log an artifact (output file)
+writeLines("Hello world!", "output.txt")
+mlflow_log_artifact("output.txt")
