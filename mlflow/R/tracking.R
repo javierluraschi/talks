40e30b999f0979b268bd423204a28c07efead3bd
Javier Luraschi <javierluraschi@hotmail.com>
2018-09-21 10:29:08 -0700

    fix user 2016 quote

--- mlflow/R/tracking.R
+++ mlflow/R/tracking.R
@@ -8,7 +8,7 @@ mlflow_log_param("param1", 5)
 # Log a metric; metrics can be updated throughout the run
 mlflow_log_metric("foo", 1)
 mlflow_log_metric("foo", 2)
-mlflow_log_metric("foo", 3)
+mlflow_log_metric("foo", 6)
 
 # Log an artifact (output file)
 writeLines("Hello world!", "output.txt")
