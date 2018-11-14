1d9a5d5efac600164d36099a4fb1f203f29b803a
Javier Luraschi <javierluraschi@hotmail.com>
2018-11-13 18:58:19 -0800

    add mlflow talk at bay area

---
+++ 2018-11-13/R/copying.R
@@ -0,0 +1,15 @@
+# !source file.copy(.file, paste0("versions/", date(), ".R"))
+
+library(mlflow)
+
+# Log a parameter (key-value pair)
+mlflow_log_param("param1", 5)
+
+# Log a metric; metrics can be updated throughout the run
+mlflow_log_metric("foo", 1)
+mlflow_log_metric("foo", 2)
+mlflow_log_metric("foo", 5)
+
+# Log an artifact (output file)
+writeLines("Hello world!", "output.txt")
+mlflow_log_artifact("output.txt")
